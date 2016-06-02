7dc759e9df69ea77d8fe4a5d4edde5207f72c720
MayukoT <tsupita@MTanemura-no-MacBook-Pro.local>
2016-06-02 23:03:42 +0900

    almost completed
    it can be compiled, and work.
    but I'll fix it a little

--- searchdic.cpp
+++ searchdic.cpp
@@ -1,17 +1,19 @@
 //searchdic.cpp
 
 #include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <fstream>
-
+#include <cstdlib>
 
 #define ILL 16
 
 using namespace std;
 
 int maxlen=0;
+ifstream dic;
 
 //these are for a new dic
 typedef pair<int,string> dicpair;
@@ -30,17 +32,29 @@ vector<dicpair> newdic;
 */
 
 //make a new dic (also has length of the word)
-int mk_newdic(ifstream ifs){
+int mk_newdic(){
   string s;
-  while(!ifs.eof()){
-    getline(ifs,s);
-    if(s.size()<=ILL){
-      newdic.push_back(dicpair(s.size(),s));
+  int i=0;
+  while(1){
+    getline(dic,s);
+    newdic.push_back(dicpair(s.size(),s));
+    cout<< newdic[i].first<<" "<<newdic[i].second<<endl;
+    i++;
+    
+    if(dic.eof()){
+      break;
     }
   }
+
   //have to make operator
   sort(newdic.begin(),newdic.end(),greater<pair<int,string> >());
   return 0;  
+}
+
+//sort charaters in the word
+string sort_letters(string inp){
+  sort(inp.begin(),inp.end());
+  return inp;
 }
 
 
@@ -53,14 +67,8 @@ string read_letters(){
     cout << "your input is:"<<endl;
     cin >> l;
   }while(l.size()!=ILL);
+  l=sort_letters(l);
   return l;
-}
-
-
-//sort charaters in the word
-string sort_letters(string inp){
-  sort(inp.begin(),inp.end());
-  return inp;
 }
 
 
@@ -104,14 +112,62 @@ int main(){
   string str;
   str=read_letters();
   ifstream dic;
-  dic.open("/usr/share/dict/words");
-  if(dic.fail()){
-    cout<<"failed to open file"<<endl;
+   dic.open("/usr/share/dict/words");
+   if(dic.fail()){
+     cout<<"failed to open file"<<endl;
     exit(EXIT_FAILURE);
+   }
+   //   mk_newdic();
+  string s;
+  while(1){
+    getline(dic,s);
+    newdic.push_back(dicpair(s.size(),s));
+    //    cout<< newdic[i].first<<" "<<newdic[i].second<<endl;
+    //i++;
+    
+    if(dic.eof()){
+      break;
+    }
   }
-  mk_newdic(dic);
-  compare(str);
+  sort(newdic.begin(),newdic.end(),greater<pair<int,string> >());
+  cout<<str<<endl;
+   // compare(str);
+ long i=0;
+  string arr;
+  while(newdic[i].first>=maxlen){
+    if(newdic[i].first>ILL){
+      i++;
+    }
+    else if(newdic[i].first==ILL){
+      arr=sort_letters(newdic[i].second);
+      if(arr==str){
+	maxlen=newdic[i].first;
+	cout<<newdic[i].second<<endl;
+      }
+      i++;
+    }      
+    else if(newdic[i].first<ILL){
+      int j=0,k=0;
+      arr=sort_letters(newdic[i].second);
+      while(k<=ILL){
+	if(arr[j]==str[k]){
+	  if(j==arr.size()){
+	    maxlen=newdic[i].first;
+	    cout<<newdic[i].second<<endl;
+	    break;
+	  }else{
+	    j++;k=0; 
+	  }	    
+	}
+	else{
+	  k++;
+	}
+      }
+      i++;      
+    }
+  }
 
   return 0;
+  
+  
 }
-
