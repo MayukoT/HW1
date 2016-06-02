59dbe34127b47a30b1f766de5ed9091e2c7f78f2
MayukoT <tsupita@MTanemura-no-MacBook-Pro.local>
2016-06-02 11:36:06 +0900

    open,length,sort are written
    some of the functions were written

--- searchdic.cpp
+++ searchdic.cpp
@@ -2,52 +2,76 @@
 
 #include<iostream>
 #include<string>
+#include<vector>
+#include<algorithm>
+#include<fstream>
 
 #define ILL 16
+#define DICLEN 1000000
 
 using namespace std;
 
-//target:
-//not sort all of the word in dic.
-//pick up a word and sort
-//
 int maxlen=0;
+
 
 class Words
 {
-public:
-  string name;
+  string wd;
   int len;
   bool jud;
+public:
   Words(){}
-  Words(string n){
-}
-
-    
+  Words(string w){
+    wd << w;
+    len << w.size();
+    if(len>ILL){jud << false;}
+    else{jud << true;}
+  }  
 };
 
-    
+//open dic
+int open_dic(){
+  ifstream dic("/usr/share/dict/words");
+  if(!dic){
+    cout<<"failed to open file"<<endl;
+    exit(EXIT_FAILURE);
+  } 
+}
 
-void compare_length(string a,string b){
+int mk_newdic(){
 
+}
+
+//length of inp-dw
+int compare_length(string inp,string dw){
+  int dif;
+  dif << (inp.size-dw.size);
+  return dif;
 }
 
 //read 16 letters from console
 //while the length is not good, repeat reading
-string read_letter(){
-  string l;
-
+string read_letters(){
+  string l;  
   do{
-  cout << "please put " << ILL <<" letters."<<endl;
-  cout << "your input is:"<<endl;
-  cin >> l;
+    cout << "please put " << ILL <<" letters."<<endl;
+    cout << "your input is:"<<endl;
+    cin >> l;
   }while(l.size()!=ILL);
-
   return l;
 }
 
-
-
-int main(){
-  read_letter();
+//sort
+string sort_letters(string inp, int ilen){
+  sort(inp.begin(),inp.end());
+  return inp;
 }
+
+//main
+int main(){
+  read_letters();
+}
+
+void compare(){
+
+} 
